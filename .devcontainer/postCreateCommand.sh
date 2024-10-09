9bb7b54546cb27ed80d5fa5bd2638fca6b6cbec4
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-10-09 22:02:40 +0800

    Fix `postCreateCommand.sh`

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set up poetry
-echo "export PYTHON_PATH='$PYTHON_PATH:$PWD'" >> ~/.bashrc
+echo "export PYTHON_PATH='$PYTHON_PATH:$PWD'" >> ~/.zshrc
 poetry install --directory=image-generation
 
-export temporary=$(poetry env info --path)
-echo "export PATH='$temporary/bin:$PATH'" >> ~/.bashrc
+export temporary=$(cd image-generation && poetry env info --path)
+echo "export PATH='$temporary/bin:$PATH'" >> ~/.zshrc
 
 # Set up aliases
-echo "alias porun='poetry run'" >> ~/.bashrc
-echo "alias ipynb='porun jupyter notebook --allow-root'" >> ~/.bashrc
-echo "alias ipylab='porun jupyter lab --allow-root'" >> ~/.bashrc
+echo "alias porun='poetry run'" >> ~/.zshrc
+echo "alias ipynb='porun jupyter notebook --allow-root'" >> ~/.zshrc
+echo "alias ipylab='porun jupyter lab --allow-root'" >> ~/.zshrc
