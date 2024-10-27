3ee2b9282532f7b43bda424d4cf5744124da9127
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-10-27 16:00:11 +0800

    Fix issue with running notebooks using VSCode

--- code/image_generation/magics/helpers.py
+++ code/image_generation/magics/helpers.py
@@ -23,22 +23,27 @@ def get_notebook_path() -> Path:
 
     # Try and detect the kernel ID from the connection file
     kernel_match = re.search(r"kernel-(.*).json", ipykernel.connect.get_connection_file())
-    if kernel_match is None:
-        # We need to fall back on getting an environment variable
-        warnings.warn("Can't detect kernel ID. Falling back on reading `NOTEBOOK_PATH` environment variable.")
-        notebook_path = os.environ.get("NOTEBOOK_PATH", None)
-        if notebook_path is None:
-            raise ValueError("Can't detect notebook path. Please set `NOTEBOOK_PATH` environment variable.")
+    if kernel_match is not None:
+        kernel_id = kernel_match.group(1)
 
-        return Path(notebook_path)
+        # Find the server with the matching kernel ID
+        servers = list_running_servers()
+        for ss in servers:
+            response = requests.get(urljoin(ss["url"], "api/sessions"), params={"token": ss.get("token", "")})
+            for nn in json.loads(response.text):
+                if nn["kernel"]["id"] == kernel_id:
+                    relative_path = nn["notebook"]["path"]
+                    return Path(ss["root_dir"], relative_path)
+        
+        # Prepare for fallback method
+        issue = "find requested"
+    else:
+        issue = "detect"
+    
+    # We need to fall back on getting an environment variable
+    warnings.warn(f"Can't {issue} kernel ID. Falling back on reading `NOTEBOOK_PATH` environment variable.")
+    notebook_path = os.environ.get("NOTEBOOK_PATH", None)
+    if notebook_path is None:
+        raise ValueError("Can't detect notebook path. Please set `NOTEBOOK_PATH` environment variable.")
 
-    kernel_id = kernel_match.group(1)
-
-    # Find the server with the matching kernel ID
-    servers = list_running_servers()
-    for ss in servers:
-        response = requests.get(urljoin(ss["url"], "api/sessions"), params={"token": ss.get("token", "")})
-        for nn in json.loads(response.text):
-            if nn["kernel"]["id"] == kernel_id:
-                relative_path = nn["notebook"]["path"]
-                return Path(ss["root_dir"], relative_path)
+    return Path(notebook_path)
