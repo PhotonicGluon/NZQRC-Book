080e3aa1394b4f8088693c3e76e81896141edbe8
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-10-27 13:15:39 +0800

    Make the `do-all` command work with the new changes

--- code/image_generation/cli/execute_notebooks.py
+++ code/image_generation/cli/execute_notebooks.py
@@ -1,3 +1,5 @@
+import os
+
 import papermill as pm
 from tqdm import tqdm
 
@@ -10,5 +12,17 @@ def execute_notebooks(*notebooks: str):
         notebooks: the names of the notebooks to execute.
     """
 
+    # Save the current working directory
+    cwd = os.getcwd()
+
+    # Update the notebook paths to be absolute
+    notebooks = [os.path.abspath(notebook) for notebook in notebooks]
+
+    # Execute notebooks
     for notebook in tqdm(notebooks, desc="Executing notebooks"):
-        pm.execute_notebook(notebook, None, progress_bar=True)
+        os.environ["NOTEBOOK_PATH"] = notebook
+        os.chdir(os.path.dirname(notebook))
+        pm.execute_notebook(os.path.basename(notebook), None, progress_bar=True)
+
+    # Reset the CWD back to original
+    os.chdir(cwd)
--- code/image_generation/magics/helpers.py
+++ code/image_generation/magics/helpers.py
@@ -1,6 +1,8 @@
 import json
+import os
 import re
 from pathlib import Path
+import warnings
 
 import ipykernel
 import requests
@@ -19,9 +21,21 @@ def get_notebook_path() -> Path:
         Adapted from https://github.com/jupyter/notebook/issues/1000#issuecomment-359875246.
     """
 
-    kernel_id = re.search(r"kernel-(.*).json", ipykernel.connect.get_connection_file()).group(1)
-    servers = list_running_servers()
+    # Try and detect the kernel ID from the connection file
+    kernel_match = re.search(r"kernel-(.*).json", ipykernel.connect.get_connection_file())
+    if kernel_match is None:
+        # We need to fall back on getting an environment variable
+        warnings.warn("Can't detect kernel ID. Falling back on reading `NOTEBOOK_PATH` environment variable.")
+        notebook_path = os.environ.get("NOTEBOOK_PATH", None)
+        if notebook_path is None:
+            raise ValueError("Can't detect notebook path. Please set `NOTEBOOK_PATH` environment variable.")
 
+        return Path(notebook_path)
+
+    kernel_id = kernel_match.group(1)
+
+    # Find the server with the matching kernel ID
+    servers = list_running_servers()
     for ss in servers:
         response = requests.get(urljoin(ss["url"], "api/sessions"), params={"token": ss.get("token", "")})
         for nn in json.loads(response.text):
