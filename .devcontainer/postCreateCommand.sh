a6afb591ab2d474eec924750d7d76f962ffb7211
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-11-23 13:24:34 +0800

    Fix newlines in `.zshrc`

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Set up poetry
-echo "\n# Path Setup" >> ~/.zshrc
+echo "" >> ~/.zshrc
+echo "# Path Setup" >> ~/.zshrc
 
 echo "export PYTHON_PATH='$PYTHON_PATH:$PWD'" >> ~/.zshrc
 poetry install --directory=code
@@ -10,19 +11,22 @@ export temporary=$(cd code && poetry env info --path)
 echo "export PATH='$temporary/bin:$PATH'" >> ~/.zshrc
 
 # Fix weird ZSH history file issue
-echo "\n# ZSH History Setup" >> ~/.zshrc
+echo "" >> ~/.zshrc
+echo "# ZSH History Setup" >> ~/.zshrc
 echo "HISTFILE=~/.histfile" >> ~/.zshrc
 echo "HISTSIZE=1000" >> ~/.zshrc
 echo "SAVEHIST=1000" >> ~/.zshrc
 echo "setopt appendhistory" >> ~/.zshrc
 
 # Set up aliases
-echo "\n# Aliases" >> ~/.zshrc
+echo "" >> ~/.zshrc
+echo "# Aliases" >> ~/.zshrc
 
 echo "alias porun='poetry run'" >> ~/.zshrc
 echo "alias ipynb='porun jupyter notebook --allow-root'" >> ~/.zshrc
 echo "alias ipylab='porun jupyter lab --allow-root'" >> ~/.zshrc
 
 # Misc
-echo "\n# Miscellaneous" >> ~/.zshrc
+echo "" >> ~/.zshrc
+echo "# Miscellaneous" >> ~/.zshrc
 echo "cd code" >> ~/.zshrc
