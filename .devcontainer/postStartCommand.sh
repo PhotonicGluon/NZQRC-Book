b6de13c662cb0415e476362efa42daf820e85ecd
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-11-27 10:20:09 +0800

    Copy Git hook files instead of making symlink

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,7 +1,7 @@
 # Ensure that a `commit.tex` file exists
 touch book/commit.tex
 
-# Set up execution permissions for pre- and post- commit hooks
-ln -sf ../../.devcontainer/hooks/pre-commit.sh .git/hooks/pre-commit
-ln -sf ../../.devcontainer/hooks/post-commit.sh .git/hooks/post-commit
+# Copy Git commit hooks and set their permissions
+cp /workspace/.devcontainer/hooks/pre-commit.sh /workspace/.git/hooks/pre-commit
+cp /workspace/.devcontainer/hooks/post-commit.sh /workspace/.git/hooks/post-commit
 chmod +x /workspace/.git/hooks/pre-commit /workspace/.git/hooks/post-commit
